b5b5ad5424169c456a091073ca510f455f91e1b4
ucmit <ulan@cmit.ru>
2020-12-20 18:27:01 +0700

    asdasdasd

--- script.js
+++ script.js
@@ -3,25 +3,50 @@ let music;
 $(document).ready(function () {
     // Настраиваем музыку
     music = new Howl({
-        src: ["audio/1.mp3"],
+        src: ["audio/1.mp3", "audio/2.mp3", "audio/3.mp3"],
         loop: true,
         volume: 0.5
     });
 });
 // Если нажали на ноту, играет музыка
 $("#tree>img").click(function(e){ 
+    music.stop();
     let name = e.target.id;
     console.log(e)
     if(name == "toy1"){
         music.play(0);
     }
-    // else if(name == "toy2"){
-    //     music.play(1);
-    // }
-    // else if(name == "toy3"){
-    //     music.play(2);
-    // }
+    else if(name == "toy2"){
+        music.play(1);
+    }
+    else if(name == "toy3"){
+        music.play(2);
+    }
 });
+
+
+// Интервал 
+let timer = setInterval(function(){
+    // Время нового года
+    let newYear = new Date("1 January 2021 00:00:00");
+    // Нынешнее время
+    let now = new Date();
+    // Разность дат
+    let diff = newYear - now;
+    console.log(diff)
+
+    let days = Math.floor( diff/(24*60*60*1000) );
+    let hours = Math.floor( (diff%(24*60*60*1000)) / (60*60*1000) );
+    let minutes = Math.floor( (diff%(60*60*1000)) / (60*1000));
+    let seconds = Math.floor( (diff%(60*1000)) / 1000);
+
+    $("#seconds").text(seconds);
+    $("#minutes").text(minutes + " :");
+    $("#hours").text(hours + " :");
+    $("#days").text(days + " :");
+
+}, 1000)
+
 
 
 
